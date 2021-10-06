f1e4c37143fe9d53eceeea43bb2733990ec9d6ba
Renat Gubaev <>
2021-10-06 18:41:20 +0300

    no message

--- api/OccultistClient.ts
+++ api/OccultistClient.ts
@@ -1,9 +1,11 @@
 import { TransportHttp, ITransportHttpSettings } from '@ts-core/common/transport/http';
 import { ILogger } from '@ts-core/common/logger';
 import * as _ from 'lodash';
-import { TraceUtil } from '@ts-core/common/trace';
+import { ITraceable, TraceUtil } from '@ts-core/common/trace';
 import { TransformUtil } from '@ts-core/common/util';
 import { IClockDto, IClockDtoResponse } from './clock/IClockDto';
+import { IInitDto, IInitDtoResponse, ILoginDto, ILoginDtoResponse } from './login';
+import { User } from '../user';
 
 export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
     // --------------------------------------------------------------------------
@@ -15,6 +17,26 @@ export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
     constructor(logger: ILogger, url?: string) {
         super(logger, { method: 'get', isHandleError: true, isHandleLoading: true, headers: {} });
         this.url = url;
+    }
+
+    // --------------------------------------------------------------------------
+    //
+    //  Auth Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public async login(data: ILoginDto): Promise<ILoginDtoResponse> {
+        return this.call<ILoginDtoResponse, ILoginDto>(LOGIN_URL, { data: TraceUtil.addIfNeed(data), method: 'post' });
+    }
+
+    public async init(data?: IInitDto): Promise<IInitDtoResponse> {
+        let item = await this.call<IInitDtoResponse, IInitDto>(INIT_URL, { data: TraceUtil.addIfNeed(data) });
+        item.user = TransformUtil.toClass(User, item.user);
+        return item;
+    }
+
+    public async logout(traceId?: string): Promise<void> {
+        return this.call<void, ITraceable>(LOGOUT_URL, { data: TraceUtil.addIfNeed({ traceId }), method: 'post' });
     }
 
     // --------------------------------------------------------------------------
@@ -46,6 +68,8 @@ export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
 }
 
 export const PREFIX_URL = 'api/';
-
+export const INIT_URL = PREFIX_URL + 'init';
+export const LOGIN_URL = PREFIX_URL + 'login';
+export const LOGOUT_URL = PREFIX_URL + 'logout';
 export const CLOCK_URL = PREFIX_URL + 'clock';
 export const LOCALE_URL = PREFIX_URL + 'locale';
