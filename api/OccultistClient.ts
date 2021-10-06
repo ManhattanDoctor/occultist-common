e7ab970a0b11f38d05c5f8b34f62c5424ec070f7
Renat Gubaev <>
2021-10-06 16:02:19 +0300

    no message

--- api/OccultistClient.ts
+++ api/OccultistClient.ts
@@ -1,12 +1,9 @@
 import { TransportHttp, ITransportHttpSettings } from '@ts-core/common/transport/http';
 import { ILogger } from '@ts-core/common/logger';
 import * as _ from 'lodash';
-import { ILoginDto, ILoginDtoResponse, IInitDto, IInitDtoResponse } from './login';
-import { ITraceable, TraceUtil } from '@ts-core/common/trace';
+import { TraceUtil } from '@ts-core/common/trace';
 import { TransformUtil } from '@ts-core/common/util';
-import { User } from '../user';
 import { IClockDto, IClockDtoResponse } from './clock/IClockDto';
-import { IGeo } from '../geo';
 
 export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
     // --------------------------------------------------------------------------
@@ -22,33 +19,12 @@ export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
 
     // --------------------------------------------------------------------------
     //
-    //  Auth Methods
-    //
-    // --------------------------------------------------------------------------
-
-    public async login(data: ILoginDto): Promise<ILoginDtoResponse> {
-        return this.call<ILoginDtoResponse, ILoginDto>(LOGIN_URL, { data: TraceUtil.addIfNeed(data), method: 'post' });
-    }
-
-    public async init(data?: IInitDto): Promise<IInitDtoResponse> {
-        let item = await this.call<IInitDtoResponse, IInitDto>(INIT_URL, { data: TraceUtil.addIfNeed(data) });
-        item.user = TransformUtil.toClass(User, item.user);
-        return item;
-    }
-
-    public async logout(traceId?: string): Promise<void> {
-        return this.call<void, ITraceable>(LOGOUT_URL, { data: TraceUtil.addIfNeed({ traceId }), method: 'post' });
-    }
-
-    // --------------------------------------------------------------------------
-    //
     //  Other Methods
     //
     // --------------------------------------------------------------------------
-
-    public async geo(data: ITraceable): Promise<IGeo> {
-        let item = await this.call<IGeo, ITraceable>(GEO_URL, { data: TraceUtil.addIfNeed(data), isHandleError: false });
-        return TransformUtil.toClass(IGeo, item);
+    
+    public async locale(locale: string): Promise<any> {
+        return this.call<any>(`${LOCALE_URL}/${locale}`);
     }
     
     public async clock(data: IClockDto): Promise<IClockDtoResponse> {
@@ -71,8 +47,5 @@ export class OccultistClient extends TransportHttp<ITransportHttpSettings> {
 
 export const PREFIX_URL = 'api/';
 
-export const GEO_URL = PREFIX_URL + 'geo';
-export const INIT_URL = PREFIX_URL + 'init';
 export const CLOCK_URL = PREFIX_URL + 'clock';
-export const LOGIN_URL = PREFIX_URL + 'login';
-export const LOGOUT_URL = PREFIX_URL + 'logout';
+export const LOCALE_URL = PREFIX_URL + 'locale';
