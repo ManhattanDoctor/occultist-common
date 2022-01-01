7378a88e8f1c6cd62a98c0ae5444917a23d76011
Renat Gubaev <renat.gubaev@gmail.com>
2022-01-01 21:48:42 +0300

    no message

--- api/user/IUserEditDto.ts
+++ api/user/IUserEditDto.ts
@@ -2,7 +2,6 @@ import { ITraceable } from '@ts-core/common/trace';
 import { User, UserAccount, UserPreferences } from '../../user';
 
 export interface IUserEditDto extends ITraceable {
-    // id: number;
     account?: Partial<UserAccount>;
     preferences?: Partial<UserPreferences>;
 }
