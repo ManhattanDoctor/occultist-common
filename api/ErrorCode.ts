25253e2d31c9c3eb9dfdf547e25251c67a62ae90
Renat Gubaev <renat.gubaev@gmail.com>
2022-01-06 19:09:09 +0300

    no message

--- api/ErrorCode.ts
+++ api/ErrorCode.ts
@@ -1,3 +1,17 @@
+import { ExtendedError } from '@ts-core/common/error';
+
 export enum ErrorCode {
+    TAROT_SPREAD_EXCEED = 'TAROT_SPREAD_EXCEED',
     TAROT_SPREAD_INVALID_DATE = 'TAROT_SPREAD_INVALID_DATE'
+}
+
+export class TarotSpreadInvalidDateError<T = { startDate: Date; finishDate: Date; currentDate: Date; date: Date }> extends ExtendedError<T, ErrorCode> {
+    constructor(details: T) {
+        super('Invalid date', ErrorCode.TAROT_SPREAD_INVALID_DATE, details);
+    }
+}
+export class TarotSpreadExceedError<T = { startDate: Date; finishDate: Date; count: Number }> extends ExtendedError<T, ErrorCode> {
+    constructor(details: T) {
+        super('Too many spreads', ErrorCode.TAROT_SPREAD_EXCEED, details);
+    }
 }
