5302c5e04a0004663e051901b6fddf3a8b21b882
Renat Gubaev <renat.gubaev@gmail.com>
2022-05-15 22:45:40 +0300

    no message

--- api/tarot/spread/ITarotSpreadEditDto.ts
+++ api/tarot/spread/ITarotSpreadEditDto.ts
@@ -2,7 +2,7 @@ import { TarotSpread, TarotSpreadDesk, TarotSpreadPrivacy } from '../../../tarot
 import { ITraceable } from '@ts-core/common/trace';
 
 export interface ITarotSpreadEditDto extends ITraceable {
-    uid: string;
+    uid?: string;
     desk?: TarotSpreadDesk;
     querent?: string;
     privacy?: TarotSpreadPrivacy
