ce7695ab5d3d5c98259ed5880b395677e195ae00
Renat Gubaev <renat.gubaev@gmail.com>
2022-05-15 13:12:37 +0300

    no message

--- api/locale/ILocaleDto.ts
+++ api/locale/ILocaleDto.ts
@@ -1,3 +1,38 @@
 export interface ILocaleDto {
     version?: string;
 }
+
+export const LOCALE_PREFIXES = [
+    '.json',
+    'Custom.json',
+
+    'User.json',
+    'Comment.json',
+
+    'Myth.json',
+    'Demons.json',
+    'Angels.json',
+    'Natures.json',
+    'Planets.json',
+    'Zodiacs.json',
+    'Sefiras.json',
+    'Columns.json',
+    'Hebrews.json',
+    'Alchemy.json',
+    'Qliphoths.json',
+
+    'Bibliography.json',
+
+    'Tarots.json',
+    'TarotsChart.json',
+    'TarotsSpread.json',
+    'TarotsHistory.json',
+    'TarotsQuestion.json',
+
+    'Clock.json',
+    'Gematria.json',
+
+    'Main.json',
+    'About.json'
+]
+
