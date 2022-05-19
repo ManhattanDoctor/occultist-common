fdb7822c06ffebfa7e04a692dbd857bf4b219797
Renat Gubaev <renat.gubaev@gmail.com>
2022-05-20 00:57:46 +0300

    no message

--- api/login/LoginResource.ts
+++ api/login/LoginResource.ts
@@ -1,6 +1,6 @@
 export enum LoginResource {
-    VK = 'VK',
+    VK_SITE = 'VK_SITE',
     VK_INTERNAL = 'VK_INTERNAL',
-    GOOGLE = 'GOOGLE',
-    FACEBOOK = 'FACEBOOK'
+    GOOGLE_SITE = 'GOOGLE_SITE',
+    FACEBOOK_SITE = 'FACEBOOK_SITE'
 }
