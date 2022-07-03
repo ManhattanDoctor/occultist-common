5e43240bca569363f107b8c6b1a5a914102c1222
Renat Gubaev <renat.gubaev@gmail.com>
2022-07-03 12:45:09 +0300

    no message

--- user/UserPreferences.ts
+++ user/UserPreferences.ts
@@ -45,10 +45,10 @@ export const USER_PREFERENCES_STRING_MAX_LENGTH = 256;
 export const USER_PREFERENCES_NAME_MIN_LENGTH = 1;
 export const USER_PREFERENCES_NAME_MAX_LENGTH = 50;
 
-export const USER_PREFERENCES_VK_PATTERN = /^https?:\/\/vk\.com\/.*/;
-export const USER_PREFERENCES_FACEBOOK_PATTERN = /^https?:\/\/facebook\.com\/.*/;
-export const USER_PREFERENCES_TELEGRAM_PATTERN = /^https?:\/\/t\.me\/.*/;
-export const USER_PREFERENCES_INSTAGRAM_PATTERN = /^https?:\/\/instagram\.com\/.*/;
+export const USER_PREFERENCES_VK_PATTERN = /^$|https?:\/\/vk\.com\/.*/;
+export const USER_PREFERENCES_FACEBOOK_PATTERN = /^$|https?:\/\/facebook\.com\/.*/;
+export const USER_PREFERENCES_TELEGRAM_PATTERN = /^$|https?:\/\/t\.me\/.*/;
+export const USER_PREFERENCES_INSTAGRAM_PATTERN = /^$|https?:\/\/instagram\.com\/.*/;
 
 export const USER_PREFERENCES_PHONE_MAX_LENGTH = 12;
 export const USER_PREFERENCES_LOCALE_MAX_LENGTH = 2;
