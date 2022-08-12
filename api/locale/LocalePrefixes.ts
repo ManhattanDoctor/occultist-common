fd3ba22854a6cbf3ee04313b5aae57f8c0975542
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-12 23:31:54 +0300

    no message

---
+++ api/locale/LocalePrefixes.ts
@@ -0,0 +1,40 @@
+
+export const LOCALE_PREFIXES = [
+    '.json',
+    'Custom.json',
+
+    'User.json',
+    'Comment.json',
+
+    'Myth.json',
+    'Ahet.json',
+    'Demons.json',
+    'Geniuses.json',
+    'Natures.json',
+    'Planets.json',
+    'Zodiacs.json',
+    'ZodiacSpirits.json',
+    'ZodiacDecanSpirits.json',
+    'Sefiras.json',
+    'Columns.json',
+    'Hebrews.json',
+    'Alchemy.json',
+    'Qliphoths.json',
+
+    'Management.json',
+    'Bibliography.json',
+
+    'Tarots.json',
+    'TarotsChart.json',
+    'TarotsSpread.json',
+    'TarotsHistory.json',
+    'TarotsSefiras.json',
+    'TarotsQuestion.json',
+
+    'Clock.json',
+    'Gematria.json',
+
+    'Main.json',
+    'About.json'
+]
+
--- api/locale/index.ts
+++ api/locale/index.ts
@@ -1,1 +1,2 @@
 export * from './ILocaleDto';
+export * from './LocalePrefixes';
