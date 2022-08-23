290ff116081ed559cdcd31662fb13fa4351f3b6e
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-23 15:46:00 +0300

    no message

--- api/management/IManagementTarotSpreadList.ts
+++ api/management/IManagementTarotSpreadList.ts
@@ -1,5 +1,4 @@
 import { ITraceable, IPaginable } from '@ts-core/common';
 import { TarotSpread } from '../../tarot';
-import { UserPreferences } from '../../user';
 
-export interface IManagementTarotSpreadListDto extends IPaginable<TarotSpread, UserPreferences>, ITraceable { }
+export interface IManagementTarotSpreadListDto extends IPaginable<TarotSpread, any>, ITraceable { }
