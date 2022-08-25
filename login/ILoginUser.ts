1d94dd1de3c19a784e38b8c120f44d834a4c8a6a
Renat Gubaev <renat.gubaev@gmail.com>
2022-08-25 22:59:41 +0300

    no message

--- login/ILoginUser.ts
+++ login/ILoginUser.ts
@@ -8,3 +8,22 @@ export class LoginUser {
 export class VkLoginUser extends LoginUser {
     params: string;
 }
+
+export class GoUser extends LoginUser {
+    //--------------------------------------------------------------------------
+    //
+    // 	Constructor
+    //
+    //--------------------------------------------------------------------------
+
+    constructor(item: any) {
+        super();
+        this.id = item.sub;
+        this.preferences = {
+            name: item.name,
+            email: item.email,
+            locale: item.locale,
+            picture: item.picture,
+        }
+    }
+}
