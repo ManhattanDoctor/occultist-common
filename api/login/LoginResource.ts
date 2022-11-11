4da631d27454b093047041476ca3f15c86cb7722
Renat Gubaev <renat.gubaev@gmail.com>
2022-11-11 19:58:05 +0300

    no message

--- api/login/LoginResource.ts
+++ api/login/LoginResource.ts
@@ -1,8 +1,8 @@
 export enum LoginResource {
     VK_INTERNAL = 'VK_INTERNAL',
 
-    VK = 'VK_SITE',
-    MAIL = 'MAIL_SITE',
-    YANDEX = 'YANDEX_SITE',
-    GOOGLE = 'GOOGLE_SITE',
+    VK = 'VK',
+    MAIL = 'MAIL',
+    YANDEX = 'YANDEX',
+    GOOGLE = 'GOOGLE',
 }
