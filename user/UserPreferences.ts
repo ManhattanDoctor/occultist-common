70a8c139cce5844e64f7dd77b78053bbfd537342
Renat Gubaev <gubaev.r@n-t.io>
2023-07-09 17:49:23 +0300

    no message

--- user/UserPreferences.ts
+++ user/UserPreferences.ts
@@ -22,6 +22,7 @@ export class UserPreferences {
     tarotDesk?: string;
 
     isHideFromPeople?: boolean;
+    isNotSuggestShowHelp?: boolean;
     isNotSuggestAddToFavorites?: boolean;
     isNotSuggestAllowSendNotifications?: boolean;
 
