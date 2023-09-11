31692f431899d65cd1471f57d0f9749aa434348f
Renat Gubaev <gubaev.r@n-t.io>
2023-09-11 17:32:32 +0300

    no message

--- api/payment/IPaymentBalanceEditDto.ts
+++ api/payment/IPaymentBalanceEditDto.ts
@@ -1,7 +1,8 @@
+import { CoinId } from '../../coin';
 import { ITraceable } from '@ts-core/common';
 
 export interface IPaymentBalanceEditDto extends ITraceable {
-    coinId: string;
+    coinId: CoinId;
     amount: string;
     userId: number;
 }
