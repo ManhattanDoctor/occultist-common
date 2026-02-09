26951b4c6b8f3889ff5ff1c9a5c4c03c59dec72d
Renat Gubaev <gubaev.r@n-t.io>
2026-02-09 09:57:09 +0300

    no message

--- api/tarot/spread/ITarotSpreadMeaningAiDto.ts
+++ api/tarot/spread/ITarotSpreadMeaningAiDto.ts
@@ -3,5 +3,3 @@ import { TarotSpreadMeaningAi } from '../../../tarot';
 
 export type ITarotSpreadMeaningAiDtoResponse = TarotSpreadMeaningAi;
 
-export const TAROT_SPREAD_MEANING_AI_TIMEOUT = 5 * DateUtil.MILLISECONDS_MINUTE;
-
